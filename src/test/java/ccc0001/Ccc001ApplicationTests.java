1a48e7e9008753b3f40be51d6b3c325c81104176
Matt Cadorette <mcadorette@gmail.com>
2018-10-17 15:52:15 -0400

    Initial commit from Atomist

---
+++ src/test/java/ccc0001/Ccc001ApplicationTests.java
@@ -0,0 +1,16 @@
+package ccc0001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ccc001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
